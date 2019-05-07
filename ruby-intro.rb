3c8884f4862be6a3ae73d691dc800942f6a737a0
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-05-07 16:26:54 -0500

    init commit, tried some stuff

---
+++ ruby-intro.rb
@@ -0,0 +1,29 @@
+puts 'Hello World!'
+
+if 1 > 2
+    puts 'one is greater than two'
+elsif 2 > 1
+    puts 'two is greater than one'
+else
+    puts 'one is equal to two'
+end
+
+def hey_ho?
+    true
+end
+
+puts 'let\'s go' if hey_ho?
+
+apples = 'oranges'
+
+puts apples
+
+number = 6
+
+puts number * 4
+
+def hi3
+    'hi' * 3
+end
+
+puts hi3
